b15e69ba734fb3735b5292580fd5799b07a22e9e
Tom BRUAIRE <tombruaire@gmail.com>
2024-11-25 15:52:39 +0100

    Installation du package @ckeditor/ckeditor5-build-classic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Inject } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements   
+AfterViewInit {
+  
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
