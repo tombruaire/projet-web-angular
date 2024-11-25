6ebc9407e545ab4d23d97bf10d435c42018427d7
Tom BRUAIRE <tombruaire@gmail.com>
2024-11-25 18:46:18 +0100

    Intégration d'un Carousel pour le listing des articles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,10 @@
-import { Component, AfterViewInit, ViewChild, ElementRef, Inject } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements   
-AfterViewInit {
-  
-  ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
-  }
+export class HomeComponent {
 
 }
