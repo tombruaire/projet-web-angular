56d05afb3fa1e91c737b6f56ae41a35b8561e463
Tom BRUAIRE <tombruaire@gmail.com>
2024-11-26 11:11:45 +0100

    Importation de la librairie animate.css (cdn)

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.component.html',
-  styleUrls: ['./connexion.component.css']
+  styleUrls: ['./connexion.component.css'],
 })
 export class ConnexionComponent {
 
